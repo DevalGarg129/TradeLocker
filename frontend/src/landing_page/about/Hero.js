cd4d939257cb902f7f7a21db9d45757974ebf21c
devalgarg129 <devalgarg978@gmail.com>
2026-05-20 16:38:21 +0530

    adding nginx

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -6,6 +6,7 @@ function Hero() {
   return (
     <div className="container">
       <div className="row p-5 mt-5 mb-5">
+      <Navbar/>
         <h1 className="fs-2 text-center">
           We pioneered the discount broking model in India
           <br />
@@ -53,6 +54,7 @@ function Hero() {
             us.
           </p>
         </div>
+        <Footer/>cl
       </div>
     </div>
   );
